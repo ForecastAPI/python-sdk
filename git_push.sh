e09ce18271c68accbf47427bda5d463ed5675eec
Miguel Álvarez <testcotter@lasserafn.dk>
2026-07-23 00:34:49 +0800

    chore: regenerate SDK from openapi.yaml v2.0.4

--- git_push.sh
+++ git_push.sh
@@ -9,12 +9,12 @@ release_note=$3
 git_host=$4
 
 if [ "$git_host" = "" ]; then
-    git_host="github.com-alvarez01337"
+    git_host="github.com"
     echo "[INFO] No command line input provided. Set \$git_host to $git_host"
 fi
 
 if [ "$git_user_id" = "" ]; then
-    git_user_id="alvarez01337"
+    git_user_id="ForecastAPI"
     echo "[INFO] No command line input provided. Set \$git_user_id to $git_user_id"
 fi
 
